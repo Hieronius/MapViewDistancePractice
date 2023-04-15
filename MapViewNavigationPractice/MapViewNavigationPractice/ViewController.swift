9b339d3b123774a87f704b51d5c087d5f767833a
Arsenty Halimovsky <ghost12@yandex.ru>
2023-04-15 12:39:22 +0300

    Create a new project

---
+++ MapViewNavigationPractice/MapViewNavigationPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapViewNavigationPractice
+//
+//  Created by Арсентий Халимовский on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
